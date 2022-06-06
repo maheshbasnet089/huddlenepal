f7d3f0646aa56c1bb1328c573c14989eb55d0085
Huddle Nepal <huddlenepal@gmail.com>
2022-06-06 16:59:25 +0545

    Update : Add Morgan

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRoute.js");
+const morgan = require("morgan");
+
 
 if ((process.env.NODE_ENV = "developement")) {
   app.use(morgan("tiny")); // logging info about user using morgan package, see more about morgan on npm registry
