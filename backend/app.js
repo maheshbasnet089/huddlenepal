650f054a6a15213f3b5d6e6e07e822311849fa41
manishbasnet089 <basnetmanish089@gmail.com>
2022-06-07 15:50:22 +0545

    Complete : Add SignUP

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,9 @@ const app = express();
 const userRouter = require("./routes/userRoute.js");
 const morgan = require("morgan");
 
+app.use(express.json());
 if ((process.env.NODE_ENV = "developement")) {
-  app.use(morgan("tiny")); // logging info about user using morgan package, see more about morgan on npm registry
+  app.use(morgan("tiny")); // logging info about user(request) using morgan package, see more about morgan on npm registry
 }
 
 //Authentication Routes
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const authController = require("../controllers/authController");
 
-router.route("signup").post(authController.signUp);
+router.route("/signup").post(authController.signUp);
 
 module.exports = router;
